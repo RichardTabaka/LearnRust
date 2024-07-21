17e085486810179a87f949f2b7153b0da900928f
Richard <richard.tabaka@outlook.com>
2024-07-21 13:50:58 -0700

    Removed ignored files

--- target/debug/deps/shell-48ad053e2d84b79f.d
+++
@@ -1,7 +0,0 @@
-/home/rst/Dev/Rust/learn-rust/target/debug/deps/libshell-48ad053e2d84b79f.rmeta: shell/src/main.rs shell/src/commands/mod.rs shell/src/commands/echo.rs
-
-/home/rst/Dev/Rust/learn-rust/target/debug/deps/shell-48ad053e2d84b79f.d: shell/src/main.rs shell/src/commands/mod.rs shell/src/commands/echo.rs
-
-shell/src/main.rs:
-shell/src/commands/mod.rs:
-shell/src/commands/echo.rs:
--- target/debug/deps/shell-6fa166446ebc0006.d
+++
@@ -1,7 +0,0 @@
-/home/rst/Dev/Rust/learn-rust/target/debug/deps/libshell-6fa166446ebc0006.rmeta: shell/src/main.rs shell/src/commands/mod.rs shell/src/commands/echo.rs
-
-/home/rst/Dev/Rust/learn-rust/target/debug/deps/shell-6fa166446ebc0006.d: shell/src/main.rs shell/src/commands/mod.rs shell/src/commands/echo.rs
-
-shell/src/main.rs:
-shell/src/commands/mod.rs:
-shell/src/commands/echo.rs:
